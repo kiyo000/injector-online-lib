22be1f33360a7cbd98bd7b390eae29da6c9b6a23
kiyo000 <kiyoshiotaa00@gmail.com>
2025-09-06 22:12:52 +0800

    Update offset.lua

--- offset.lua
+++ offset.lua
@@ -22,8 +22,8 @@ offsets = {
   },
   fps_booster = {
     {lib = "libunity.so", offset = 0x8830880, hex = "h200080D2C0035FD6"},
-    {lib = "libunity.so", offset = 0x88308fc, hex = "h200080D2C0035FD6"},
-    {lib = "libunity.so", offset = 0x882fac0, hex = "h200080D2C0035FD6"}
+    {lib = "libunity.so", offset = 0x88308FC, hex = "h200080D2C0035FD6"},
+    {lib = "libunity.so", offset = 0x882FAC0, hex = "h200080D2C0035FD6"}
   },
   smooth_fps = {
     {lib = "libunity.so", offset = 0x883BCD0, hex = "h200080D2C0035FD6"},
@@ -49,34 +49,34 @@ offsets = {
     {lib = "libunity.so", offset = 0x882FEBC, hex = "h200080D2C0035FD6"}
   },
   wallhack = {
-    {lib = "libunity.so", offset = 0x91C3010, hex = "h 1F 20 03 D5 E0 03 13 AA"}
+    {lib = "libunity.so", offset = 0x91C3010, hex = "h1F2003D5E00313AA"}
   },
   no_recoil = {
-    {lib = "libunity.so", offset = 0x9fe9618, hex = "h004C40BCC0035FD6"}
+    {lib = "libunity.so", offset = 0x9FE9618, hex = "h004C40BCC0035FD6"}
   },
   no_spread = {
-    {lib = "libunity.so", offset = 0x9fe7d88, hex = "h000080D2C0035FD6"}
+    {lib = "libunity.so", offset = 0x9FE7D88, hex = "h000080D2C0035FD6"}
   },
   no_shake = {
-    {lib = "libunity.so", offset = 0x9455ca0, hex = "h010080D2C0035FD6"}
+    {lib = "libunity.so", offset = 0x9455CA0, hex = "h010080D2C0035FD6"}
   },
   hitbox = {
-    {lib = "libunity.so", offset = 0x89164a0, hex = "h200180D2C0035FD6"}
+    {lib = "libunity.so", offset = 0x89164A0, hex = "h200180D2C0035FD6"}
   },
   no_reload = {
-    {lib = "libunity.so", offset = 0xa28c574, hex = "h002C40BCC0035FD6"}
+    {lib = "libunity.so", offset = 0xA28C574, hex = "h002C40BCC0035FD6"}
   },
   fast_scope = {
-    {lib = "libunity.so", offset = 0xa28a544, hex = "h002C40BCC0035FD6"}
+    {lib = "libunity.so", offset = 0xA28A544, hex = "h002C40BCC0035FD6"}
   },
   no_parachute = {
-    {lib = "libunity.so", offset = 0x9BBA1B4, hex = "h00 00 80 D2 C0 03 5F D6"}
+    {lib = "libunity.so", offset = 0x9BBA1B4, hex = "h000080D2C0035FD6"}
   },
   no_wingsuit = {
-    {lib = "libunity.so", offset = 0x9BBA1B4, hex = "h00 00 80 D2 C0 03 5F D6"}
+    {lib = "libunity.so", offset = 0x9BBA1B4, hex = "h000080D2C0035FD6"}
   },
   br_tags = {
-    {lib = "libunity.so", offset = 0x56b67dc, hex = "h200080D2C0035FD6"}
+    {lib = "libunity.so", offset = 0x56B67DC, hex = "h200080D2C0035FD6"}
   },
   advance_uav = {
     {lib = "libunity.so", offset = 0x4C0936C, hex = "h200080D2C0035FD6"}
@@ -85,25 +85,24 @@ offsets = {
     {lib = "libunity.so", offset = 0x87871D4, hex = "h200080D2C0035FD6"}
   },
   skip_tutorial = {
-    {lib = "libunity.so", offset = 0x8abf5dc, hex = "h00008052c0035fd6"},
-    {lib = "libunity.so", offset = 0x8ac523c, hex = "h00008052c0035fd6"},
-    {lib = "libunity.so", offset = 0xae9f24c, hex = "hc0035fd6"}
+    {lib = "libunity.so", offset = 0x8ABF5DC, hex = "h00008052C0035FD6"},
+    {lib = "libunity.so", offset = 0x8AC523C, hex = "h00008052C0035FD6"},
+    {lib = "libunity.so", offset = 0xAE9F24C, hex = "hC0035FD6"}
   },
   buff_damage = {
     {lib = "libunity.so", offset = 0x9FE23EC, hex = "h200080D2C0035FD6"}
   },
   walk_underwater = {
-    {lib = "libunity.so", offset = 0x56F7604, hex = "h20 00 80 D2 C0 03 5F D6"},
-    {lib = "libunity.so", offset = 0x5713CF8, hex = "h20 00 80 D2 C0 03 5F D6"},
-    {lib = "libunity.so", offset = 0x8E8E830, hex = "h20 00 80 D2 C0 03 5F D6"}
+    {lib = "libunity.so", offset = 0x56F7604, hex = "h200080D2C0035FD6"},
+    {lib = "libunity.so", offset = 0x5713CF8, hex = "h200080D2C0035FD6"},
+    {lib = "libunity.so", offset = 0x8E8E830, hex = "h200080D2C0035FD6"}
   },
   no_flashbang = {
-    {lib = "libunity.so", offset = 0x91EEE14, hex = "h00 2C 40 BC C0 03 5F D6"}
+    {lib = "libunity.so", offset = 0x91EEE14, hex = "h002C40BCC0035FD6"}
   },
-  -- Adjustable features (these use dynamic hex, so keep as is, but offsets here)
-  jump = {lib = "libunity.so", offset = 0x654AEF0, base_hex = "h40 00 00 1C C0 03 5F D6"},
-  speed = {lib = "libunity.so", offsets = {0x908A95C, 0x908A688}, base_hex = "h40 00 00 1C C0 03 5F D6"},
-  slide = {lib = "libunity.so", offset = 0x9103DD4, base_hex = "h40 00 00 1C C0 03 5F D6"},
-  snowboard = {lib = "libunity.so", offsets = {0x90E5ABC, 0x90E3B18, 0x90E3E14, 0x90E5B0C, 0x908A5C8, 0x90E5B5C, 0x90E5BBC, 0x90E5C0C, 0x90E4040, 0x90E3CD8}, base_hex = "h40 00 00 1C C0 03 5F D6"},
-  aimbot = {lib = "libunity.so", offsets = {0xA2B2AB8, 0x875B044}, base_hex = "h40 00 00 1C C0 03 5F D6"}
+  jump = {lib = "libunity.so", offset = 0x654AEF0, base_hex = "h4000001CC0035FD6"},
+  speed = {lib = "libunity.so", offsets = {0x908A95C, 0x908A688}, base_hex = "h4000001CC0035FD6"},
+  slide = {lib = "libunity.so", offset = 0x9103DD4, base_hex = "h4000001CC0035FD6"},
+  snowboard = {lib = "libunity.so", offsets = {0x90E5ABC, 0x90E3B18, 0x90E3E14, 0x90E5B0C, 0x908A5C8, 0x90E5B5C, 0x90E5BBC, 0x90E5C0C, 0x90E4040, 0x90E3CD8}, base_hex = "h4000001CC0035FD6"},
+  aimbot = {lib = "libunity.so", offsets = {0xA2B2AB8, 0x875B044}, base_hex = "h4000001CC0035FD6"}
 }
